00e0e0e0afa46334b9b2bbc2deec4d1220c873f7
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2020-09-20 16:50:15 +0300

    Create PropsDisplayer.js

---
+++ PropsDisplayer.js
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom';
+
+class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render goes here:
+ReactDOM.render(<PropsDisplayer />, document.getElementById('app'));
